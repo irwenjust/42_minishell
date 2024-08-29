b1abd6529065e110f327a7ed9f8b1e5e8e223730
Ying Zhan <153290203+yingzhan11@users.noreply.github.com>
2024-08-29 14:30:28 +0300

    Update signal.c

--- src/signal.c
+++ src/signal.c
@@ -1,12 +1,27 @@
 #include "minishell.h"
 
-/*void    handle_sigint(int signal)
+/*
+rl_replace_line(const char *text, int clear_undo);
+    text is used to replace current line, clear_undo is 0 means not to clear the list. 
+    this func is use to clear current command line.
+rl_on_new_line(void);
+    tell readline lib now is on a new line, and func will restart for new command.
+rl_redisplay(void);
+    display the readline buffer on screen.
+*/
+void    handle_sigint(int signal)
 {
-    
-}*/
+    if (signal == SIGINT)
+    {
+        rl_replace_line("", 0);
+        printf("\n");
+        rl_on_new_line();
+        rl_redisplay();
+    }
+}
 
 void singal_default(void)
 {
-    signal(SIGINT, SIG_IGN);
+    signal(SIGINT, handle_sigint);
     signal(SIGQUIT, SIG_IGN);
 }
