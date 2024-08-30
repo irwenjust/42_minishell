223373625ef0d1e466f7cdd9c8e8b05378d74d60
Lingyi Kong <35918898+irwenjust@users.noreply.github.com>
2024-08-30 10:47:01 +0300

    Fix the exit function

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: likong <likong@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:45:27 by likong            #+#    #+#             */
-/*   Updated: 2024/08/29 14:42:36 by likong           ###   ########.fr       */
+/*   Updated: 2024/08/30 10:44:45 by likong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ static int	check_status(char *str)
 	if (ft_strlen(str) > 19)
 		return (1);
 	if (sig)
-		return (ft_strcmp(str, "9223372036854775808") <= 0);
+		return (ft_strcmp(str, "9223372036854775808") > 0);
 	else
-		return (ft_strcmp(str, "9223372036854775807") <= 0);
+		return (ft_strcmp(str, "9223372036854775807") > 0);
 }
 
 void	ft_exit(char **strs)
@@ -43,13 +43,16 @@ void	ft_exit(char **strs)
 	}
 	if (matrix_size(strs) == 2 && check_status(strs[1]))
 	{
-		ft_putstr_fd("minishell: exit: ", 2);
+		ft_putstr_fd("exit\nminishell: exit: ", 2);
 		ft_putstr_fd(strs[1], 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
 		ms()->exit = 2;
 	}
 	else if (matrix_size(strs) == 2)
+	{
+		ft_putstr_fd("exit\n", 1);
 		ms()->exit = ft_atoi(strs[1]) % 256;
+	}
 	delete_matrix(strs);
 	clean_all(true);
 }
